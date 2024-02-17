551f15f71e99dfe35b8a0c8e3dbe2ada969f0978
Owen Miller <owen9825@gmail.com>
2024-02-17 12:44:05 +1100

    Initial ballot animation

---
+++ scripts/tilting.js
@@ -0,0 +1,23 @@
+// https://github.com/gijsroge/tilt.js?tab=readme-ov-file
+
+(function ($) {
+
+    "use strict";
+
+    $(".ballot").tilt({
+        maxTilt: 15,
+        perspective: 1400,
+        easing: "cubic-bezier(.03,.98,.52,.99)",
+        speed: 300,
+        glare: true,
+        maxGlare: 0.2,
+        scale: 0.9,
+        reset: false
+    }).on('tilt.mouseEnter', function() {
+        console.log("Mouse over tilting!");
+        // Remove initial tilt when tilt.js takes over
+        $(this).css('transform','');
+        $(this).removeClass('meander'); // The animation interferes with tilt.js' interaction with the user.
+    });
+
+}(jQuery));
