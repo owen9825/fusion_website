2273d7385aa2bdfb194af51d1325073cd798e71b
Owen Miller <owen9825@gmail.com>
2024-03-31 14:55:51 +1100

    Updating sidebar nav style

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,15 +1,20 @@
 // https://codepen.io/andiio/pen/ARxgGb
 // Notice that this depends on jQuery.
 
-$(function(){
+function remToPx(rem) {
+  const rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+  return rem * rootFontSize;
+}
+
+$(function () {
     // https://stackoverflow.com/a/19561886/1495729
     //  This is needed for mobile.
-    $('.dropdown-toggle').hover(function() {
-        $(this).addClass('open');  // Notice that these are not Popper classes
-    },
-    function() {
-        $(this).removeClass('open');
-    });
+    $('.dropdown-toggle').hover(function () {
+            $(this).addClass('open');  // Notice that these are not Popper classes
+        },
+        function () {
+            $(this).removeClass('open');
+        });
 });
 
 const isMouseCloseBelowElement = (element, event) => {
@@ -17,32 +22,43 @@ const isMouseCloseBelowElement = (element, event) => {
     const parent = element.parentElement;  // <li class="nav-item dropdown>
     const parentStyle = window.getComputedStyle(parent, null);
     const paddingBottom = parseInt(parentStyle.getPropertyValue('padding-bottom'));
-    console.log(`Rect left: ${rect.left}, event location: (${event.clientX}, ${event.clientY})`);
-    return rect.bottom + paddingBottom + 2 > event.clientY  &&
+    return rect.bottom + paddingBottom + 2 > event.clientY &&
         event.clientY > rect.top &&
         rect.left <= event.clientX &&
         event.clientX <= rect.right;
 }
 
-$(document).ready(function () {
-        const nearTop = 300;  // We set our header to have padding-top of 5rem == 80px.
-        $('html').on('DOMMouseScroll mousewheel', function (e) {
-            if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-                //scrolling down
-                $("#header-nav").addClass("hide-nav-bar");
-            } else {
-                //scrolling up
-                $("#header-nav").removeClass("hide-nav-bar");
-                console.log(`We are at ${window.scrollY}`);
-            }
-            if (window.scrollY < nearTop) {
+$(document).ready(
+    function () {
+        const nearTop = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--near-top'));
+
+        function updateNavColor(targetHeight) {
+            if (targetHeight < nearTop) {
                 $("#header-nav").removeClass("bg-glass-opaque");
             } else {
                 $("#header-nav").addClass("bg-glass-opaque");
             }
+        }
+
+        function hideNav() {
+            $("#header-nav").addClass("hide-nav-bar");
+        }
+
+        function showNav() {
+            $("#header-nav").removeClass("hide-nav-bar");
+        }
+
+        $('html').on('DOMMouseScroll mousewheel', function (e) {
+            if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+                //scrolling down
+                hideNav();
+            } else {
+                //scrolling up
+                showNav();
+            }
+            updateNavColor(window.scrollY);
         });
         $('.navbar-toggler').on('click', function (event) {
-            console.log('You clicked the navbar-toggler');
             if ($(this).hasClass('clicked-open')) {
                 $(this).removeClass('clicked-open');
                 if (window.scrollY < nearTop) {
@@ -58,7 +74,7 @@ $(document).ready(function () {
         // what Popper would do, for a click.
         var dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
-        dropdownToggles.forEach(function(toggle) {
+        dropdownToggles.forEach(function (toggle) {
             var dropdownUnorderedList = toggle.nextElementSibling;
             var parentLi = toggle.parentElement;
             const revealDropdown = () => {
@@ -68,12 +84,12 @@ $(document).ready(function () {
                 // For position.
                 dropdownUnorderedList.setAttribute('data-bs-popper', 'static');
             }
-            parentLi.addEventListener('mouseenter', function(event) {
+            parentLi.addEventListener('mouseenter', function (event) {
                 if (!dropdownUnorderedList.classList.contains('show')) {
                     // Close any other dropdowns
                     var openDropdowns = document.querySelectorAll('.dropdown-menu.show');
                     var reveal = true;
-                    openDropdowns.forEach(function(openDropdown) {
+                    openDropdowns.forEach(function (openDropdown) {
                         if (dropdownUnorderedList !== openDropdown) {
                             var toggleTarget = openDropdown.previousElementSibling;
                             if (toggleTarget.classList.contains('clickedOpen')) {
@@ -89,20 +105,18 @@ $(document).ready(function () {
                     }
                 }
             });
-            toggle.addEventListener('click', function(event) {
+            toggle.addEventListener('click', function (event) {
                 if (toggle.classList.contains('clicked-open')) {
                     // Popper has processing the click first, so we can't use 'show'
-                    console.log('Closing the dropdown');
                     toggle.classList.remove('show');
                     toggle.classList.remove('clicked-open');
                     toggle.removeAttribute('data-bs-popper');
                 } else {
-                    console.log('Opening the menu');
                     revealDropdown();
                     toggle.classList.add('clicked-open');
                 }
             });
-            parentLi.addEventListener('mouseleave', function(event) {
+            parentLi.addEventListener('mouseleave', function (event) {
                 // https://stackoverflow.com/a/57321303/1495729
                 if (!dropdownUnorderedList.classList.contains('open')
                     && !isMouseCloseBelowElement(toggle, event)) {
@@ -112,5 +126,11 @@ $(document).ready(function () {
                 }
             });
         });
+
+        function handleFragmentLinkClick(event) {
+            hideNav();
+        }
+
+        $('a[href^="#"]').on('click', handleFragmentLinkClick);
     }
 );
